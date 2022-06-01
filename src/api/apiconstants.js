3da2898f3b58c2edf345c9570b7e661172dcf5c7
hitesh <hiteshwork07@gmail.com>
2022-06-01 19:00:15 +0530

    chore: url

--- src/api/apiconstants.js
+++ src/api/apiconstants.js
@@ -3,8 +3,8 @@
 //BASE
 export const API_URL = 'https://simpler-dispatch.herokuapp.com/api/v1/';
 
-export const GET_CREWRIDE_URL = API_URL + 'ride/crew';
+export const GET_CREWRIDE_URL = API_URL + 'rides/crew';
 export const GET_USER_URL = API_URL + 'user/me';
-export const UPDATE_STATUS_URL = API_URL + 'ride/status';
+export const UPDATE_STATUS_URL = API_URL + 'rides/status';
 //Auth Routes
 export const LOGIN_URL = API_URL + 'user/login/crew';
